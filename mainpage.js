b55c47942628b924a86e74c26875fbd398e1da33
xixi <xixiwangcici@gmail.com>
2016-01-19 07:10:25 +0000

    remove node_moduels, add Xixi's change to mainpage.js

---
+++ mainpage.js
@@ -0,0 +1,15 @@
+function showQuestions(string project) {
+  switch (project) {
+    case "bathroom remodeling":
+      console.log("bathroom remodeling");
+      // show questions
+      break;
+    case "roofing services":
+      console.log("roofing services");
+      // show questions
+      break;
+    default:
+      console.log("not supported projects");
+      return;
+  }
+}
